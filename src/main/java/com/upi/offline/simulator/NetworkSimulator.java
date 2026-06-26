8b67cae545238f791b50ac6372cf6d6c33e2c37d
anushka0809 <anushkab.23.beec@acharya.ac.in>
2026-06-27 01:53:04 +0530

    Added offline network simulator

---
+++ src/main/java/com/upi/offline/simulator/NetworkSimulator.java
@@ -0,0 +1,27 @@
+package com.upi.offline.simulator;
+
+import com.upi.offline.entity.Transaction;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class NetworkSimulator {
+
+    private final Random random = new Random();
+
+    public Transaction simulate(Transaction transaction) {
+
+        int hops = random.nextInt(5) + 1;
+
+        transaction.setHopCount(hops);
+
+        if (hops <= 3) {
+            transaction.setStatus("DELIVERED_OFFLINE");
+        } else {
+            transaction.setStatus("WAITING_FOR_SYNC");
+        }
+
+        return transaction;
+    }
+}
