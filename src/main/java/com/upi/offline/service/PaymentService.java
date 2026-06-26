9b166b9bd1df8377346f60c7e804ab6f21a7caf8
anushka0809 <anushkab.23.beec@acharya.ac.in>
2026-06-27 01:52:51 +0530

    Implemented payment service

---
+++ src/main/java/com/upi/offline/service/PaymentService.java
@@ -0,0 +1,45 @@
+package com.upi.offline.service;
+
+import com.upi.offline.encryption.AESEncryption;
+import com.upi.offline.entity.Transaction;
+import com.upi.offline.repository.TransactionRepository;
+import com.upi.offline.simulator.NetworkSimulator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private TransactionRepository repository;
+
+    @Autowired
+    private NetworkSimulator simulator;
+
+    public Transaction save(Transaction transaction) {
+
+        String payload = transaction.getSender()
+                + "|"
+                + transaction.getReceiver()
+                + "|"
+                + transaction.getAmount();
+
+        String encrypted = AESEncryption.encrypt(payload);
+
+        transaction.setEncryptedPayload(encrypted);
+
+        simulator.simulate(transaction);
+
+        return repository.save(transaction);
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return repository.findAll();
+    }
+
+    public Transaction getTransaction(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+}
