a42b1fc6d7f90d818e810cedb238297f057c5400
anushka0809 <anushkab.23.beec@acharya.ac.in>
2026-06-27 01:48:21 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/upi/offline/OfflineUpiApplication.java
@@ -0,0 +1,13 @@
+package com.upi.offline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflineUpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfflineUpiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upi/offline/OfflineUpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upi.offline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflineUpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
